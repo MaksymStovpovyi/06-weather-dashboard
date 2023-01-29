ad0f48b5144d68c6b2b005d0f0d5f52c2a4abb84
maxSt <stovpovymax@gmail.com>
2023-01-28 23:30:36 -0500

    function getFive done

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,7 @@ function getOne (nameAtr) {
         <h2>${data.name} (${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()})
         <img src="${iconUrl}"></h2>
         <p>Temp: ${data.main.temp} F</p>
-        <p>Wind: ${data.wind.gust} MPH</p>
+        <p>Wind: ${data.wind.speed} MPH</p>
         <p>Humidity: ${data.main.humidity} %</p>
         `;
 
@@ -35,6 +35,8 @@ function getOne (nameAtr) {
 }
 
 function getFive (nameAtr) {
+    let fiveDaysElem = document.querySelector('.five-days');
+    fiveDaysElem.innerHTML = '';
 
     let ulr = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=3058b8e0dab683dade99f6a9ab474de8`;
     fetch(ulr)
@@ -42,7 +44,27 @@ function getFive (nameAtr) {
         return response.json();
     })
     .then(function (data) {
-        console.log('getFive');
-        console.log(data);
+
+        for (let i = 0; i < data.list.length; i ++) {
+            if (data.list[i].dt_txt.includes('12')) {
+
+                let date = new Date(data.list[i].dt * 1000);
+
+                let icon = data.list[i].weather[0].icon;
+                let iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`
+
+                
+                let div = document.createElement('div');
+                div.innerHTML = `
+                <h3>${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}</h3>
+                <img src="${iconUrl}">
+                <p>Temp: ${data.list[i].main.temp} F</p>
+                <p>Wind: ${data.list[i].wind.speed} MPH</p>
+                <p>Humidity: ${data.list[i].main.humidity} %</p>
+                `;
+
+                fiveDaysElem.appendChild(div);
+            }
+        }
     })
 }
