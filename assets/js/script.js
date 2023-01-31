581016a80dae5bf5fb4b48c05e8acc72a7fe0b79
maxSt <stovpovymax@gmail.com>
2023-01-30 21:19:59 -0500

    added css styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,25 @@ historyLinksStart();
 document.getElementById('search-btn').addEventListener( 'click', (e) => {
     e.preventDefault();
     cityName = document.getElementById('search-city').value;
-    getOne(cityName);
-    getFive(cityName);
-    cityList(cityName);
-    historyLinks ();
+
+    if (cityName) {
+        getOne(cityName);
+        getFive(cityName);
+        cityList(cityName);
+        historyLinks ();
+    
+        let border = document.querySelector('.city-info');
+        border.classList.add('border');
+    
+        let ulBorder = document.querySelector('ul');
+        ulBorder.classList.add('ul-border');
+    }
 })
 
 // GET one day
 function getOne (atr) {
     
-    let ulr = `https://api.openweathermap.org/data/2.5/weather?q=${atr}&appid=3058b8e0dab683dade99f6a9ab474de8`;
+    let ulr = `https://api.openweathermap.org/data/2.5/weather?q=${atr}&appid=3058b8e0dab683dade99f6a9ab474de8&units=imperial`;
     fetch(ulr)
     .then(function (response) {
         return response.json();
@@ -25,15 +34,17 @@ function getOne (atr) {
         let date = new Date(data.dt * 1000);
         
         let title = `
-        <h2>${data.name} (${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()})
-        <img src="${iconUrl}"></h2>
-        <p>Temp: ${data.main.temp} °F</p>
-        <p>Wind: ${data.wind.speed} MPH</p>
-        <p>Humidity: ${data.main.humidity} %</p>
+            <h2>${data.name} (${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()})
+            <img src="${iconUrl}"></h2>
+            <p>Temp: ${data.main.temp} °F</p>
+            <p>Wind: ${data.wind.speed} MPH</p>
+            <p>Humidity: ${data.main.humidity} %</p>
         `;
 
         document.getElementsByClassName('city-info-title')[0].innerHTML = title;
     })
+
+    
 }
 
 // GET five days
@@ -41,7 +52,7 @@ function getFive (atr) {
     let fiveDaysElem = document.querySelector('.five-days');
     fiveDaysElem.innerHTML = '';
 
-    let ulr = `https://api.openweathermap.org/data/2.5/forecast?q=${atr}&appid=3058b8e0dab683dade99f6a9ab474de8`;
+    let ulr = `https://api.openweathermap.org/data/2.5/forecast?q=${atr}&appid=3058b8e0dab683dade99f6a9ab474de8&units=imperial`;
     fetch(ulr)
     .then(function (response) {
         return response.json();
@@ -59,11 +70,11 @@ function getFive (atr) {
                 
                 let div = document.createElement('div');
                 div.innerHTML = `
-                <h3>${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}</h3>
-                <img src="${iconUrl}">
-                <p>Temp: ${data.list[i].main.temp} °F</p>
-                <p>Wind: ${data.list[i].wind.speed} MPH</p>
-                <p>Humidity: ${data.list[i].main.humidity} %</p>
+                    <h4>${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}</h4>
+                    <img src="${iconUrl}">
+                    <p>Temp: ${data.list[i].main.temp} °F</p>
+                    <p>Wind: ${data.list[i].wind.speed} MPH</p>
+                    <p>Humidity: ${data.list[i].main.humidity} %</p>
                 `;
 
                 fiveDaysElem.appendChild(div);
@@ -87,7 +98,7 @@ function cityList(atr) {
     } else {
         arrLS = JSON.parse(localStorage.getItem('historyList'));
         arrLS.unshift(atr);
-        if (arrLS.length > 3) {
+        if (arrLS.length > 10) {
             arrLS.pop();
         }
         localStorage.setItem('historyList', JSON.stringify(arrLS));
@@ -103,7 +114,6 @@ function historyLinks () {
     let liLink = document.querySelectorAll('li');
     for (let i = 0; i < liLink.length; i++) {
         liLink[i].addEventListener('click', () => {
-            console.log(liLink[i].textContent);
             getOne(liLink[i].textContent);
             getFive(liLink[i].textContent);
         });
